c5d3f5fbf54550099858fe0e756201bda210a67d
BroodLord <DKavanagh2@UCLan.ac.uk>
2019-02-21 15:00:31 +0000

    Update2

--- Assignment/C++Ass/C++Ass/C++Ass.cpp
+++ Assignment/C++Ass/C++Ass/C++Ass.cpp
@@ -4,16 +4,12 @@
 #include "stdafx.h"
 #include <fstream>
 #include <vector>
+#include <memory>
 #include <iostream>
 #include <ctime>
 #include "defs.h"
 
 using namespace std;
-
-int Random()
-{
-	return static_cast<int>(static_cast<double> (rand()) / (RAND_MAX + 1) * 6.0f + 1);
-}
 
 int seed;
 vector <cTileClassParent> Map;
@@ -24,10 +20,17 @@ int main()
 	LoadSeed("seed.txt", seed);
 	LoadMap("Monopoly.txt", Map);
 	srand(seed);
+	unique_ptr <cTileClassParent> prt = make_unique<cTileClassParent>();
+	*prt = Map.front();
+	int playerCount = 0;
 	for (int i = 0; i < 20; i++)
 	{
-		cout << "Rolls " << ": " << Random() << endl;
-		i + Random();
+		int roll = Random();
+		cout << "Player Rolls " << roll << endl;
+		playerCount = playerCount + roll;
+		Map.at(playerCount);
+		cout << "Player Land On" << endl;//  << endl;
+		system("pause");
 	}
 	system("pause");
 }
